e1de7db7b5b1a0f37d57da965695a255062d394d
Wellington <wellingtonmachioni@gmail.com>
2022-09-14 19:50:39 -0300

    refactor(rota products): Ajustado status de retorno

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import productsService from '../services/productsService';
 
 const create = async (req: Request, res: Response): Promise<Response> => {
   const result = await productsService.create(req.body);
-  return res.status(200).json(result);
+  return res.status(StatusCodes.CREATED).json(result);
 };
 
 export default { create };
