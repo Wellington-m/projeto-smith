b9a50118a642f37b3f375528e1ad036ccc4442e3
Wellington <wellingtonmachioni@gmail.com>
2022-09-14 19:47:11 -0300

    refactor(rota products): Ajustado funções create

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -2,7 +2,7 @@ import { ResultSetHeader } from 'mysql2/promise';
 import IProduct from '../interfaces/productInterface';
 import connection from './connection';
 
-const create = async (product: IProduct) => {
+const create = async (product: IProduct): Promise<ResultSetHeader> => {
   const [result] = await connection.execute<ResultSetHeader>(`
   INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);`, [product.name, product.amount]);
   return result;
--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,7 +1,14 @@
 import IProduct from '../interfaces/productInterface';
+import productModel from '../models/productModel';
 
 const create = async (product: IProduct) => {
-  console.log(product);
+  const { insertId } = await productModel.create(product);
+  const result: IProduct = {
+    id: insertId,
+    name: product.name,
+    amount: product.amount,
+  };
+  return result;
 };
 
 export default { create };
