f50155ff2c95055d01f82bcd6cb7afc075a26c62
Wellington <wellingtonmachioni@gmail.com>
2022-09-14 20:20:22 -0300

    feat(rota products): Criado esqueleto da função para listar todos os produtos na camada controller

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -2,9 +2,14 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import productsService from '../services/productsService';
 
+const findAll = async (req: Request, res: Response): Promise<Response> => {
+  const result = await productsService.findAll();
+  return res.status(StatusCodes.OK).json(result);
+};
+
 const create = async (req: Request, res: Response): Promise<Response> => {
   const result = await productsService.create(req.body);
   return res.status(StatusCodes.CREATED).json(result);
 };
 
-export default { create };
+export default { findAll, create };
--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -4,5 +4,6 @@ import productsController from '../controllers/productsController';
 const productsRoute = Router();
 
 productsRoute.post('/', productsController.create);
+productsRoute.get('/', productsController.findAll);
 
 export default productsRoute;
