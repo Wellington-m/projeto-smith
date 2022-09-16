ec7f00774b1ee933c438532e9684558bff16e36c
Wellington <wellingtonmachioni@gmail.com>
2022-09-16 13:43:28 -0300

    refactor(rota user): Tipado retorno da função na camada controller e service

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import userService from '../services/userService';
 
-const create = async (req: Request, res: Response) => {
+const create = async (req: Request, res: Response): Promise<Response> => {
   const { username, classe, level, password } = req.body;
   const result = await userService.create({ username, classe, level, password });
   return res.status(StatusCodes.CREATED).json({ token: result });
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,7 +2,7 @@ import IUserCreate from '../interfaces/userCreateInterface';
 import userModel from '../models/userModel';
 import tokenHelper from '../helpers/token';
 
-const create = async (userInfo: IUserCreate) => {
+const create = async (userInfo: IUserCreate): Promise<string> => {
   const { insertId } = await userModel.create(userInfo);  
   const token = tokenHelper.createToken({ id: insertId });
   return token;
