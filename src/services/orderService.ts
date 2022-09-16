f84ac31f75174d5b938d676b97e9be09c44104a9
Wellington <wellingtonmachioni@gmail.com>
2022-09-16 14:48:18 -0300

    refactor(rota orders): Tipado retorno da função listAllOrders na camada service

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,6 +1,7 @@
+import IOrderReturn from '../interfaces/orderReturnInterface';
 import orderModel from '../models/orderModel';
 
-const listAllOrders = async () => {
+const listAllOrders = async (): Promise<IOrderReturn[]> => {
   const result = await orderModel.listAllOrders();
   return result;
 };
