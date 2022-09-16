82fa8f8f574ee54222a1b9cbdbe81eeeb3e2a639
Wellington <wellingtonmachioni@gmail.com>
2022-09-16 14:27:51 -0300

    feat(rota orders): Criado função para listar todas as orders na camada model

---
+++ src/models/orderModel.ts
@@ -0,0 +1,20 @@
+import { RowDataPacket } from 'mysql2';
+import connection from './connection';
+
+const listAllOrders = async () => {
+  const [result] = await connection.execute<RowDataPacket[]>(`
+  SELECT 
+  orders.id AS id,
+    userId,
+    JSON_ARRAYAGG(products.id) AS productsIds
+FROM 
+  Trybesmith.Orders AS orders
+INNER JOIN Trybesmith.Products AS products
+ON 
+  orders.id = products.orderId
+GROUP BY
+  orders.id;`);
+  return result;
+};
+
+export default { listAllOrders };
