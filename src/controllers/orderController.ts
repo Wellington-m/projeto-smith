8ec13da8391f2f4bd89edfc791b0356981db4dba
Wellington <wellingtonmachioni@gmail.com>
2022-09-16 14:49:07 -0300

    refactor(rota orders): Tipado retorno da função listAllOrders na camada controller

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import orderService from '../services/orderService';
 
-const listAllOrders = async (_req: Request, res: Response) => {
+const listAllOrders = async (_req: Request, res: Response): Promise<Response> => {
   const result = await orderService.listAllOrders();
   return res.status(StatusCodes.OK).json(result);
 };
