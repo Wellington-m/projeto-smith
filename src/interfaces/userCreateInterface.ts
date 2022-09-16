33a3a5f5b3d2f9a55428f035e879c0a48458b5b1
Wellington <wellingtonmachioni@gmail.com>
2022-09-16 13:24:38 -0300

    feat(rota user): Criado função para cadastro de novo usuário na camada model

---
+++ src/interfaces/userCreateInterface.ts
@@ -0,0 +1,8 @@
+interface IUserCreate { 
+  username: string,
+  classe: string,
+  level: number,
+  password: string
+}
+
+export default IUserCreate;
---
+++ src/models/userModel.ts
@@ -0,0 +1,15 @@
+import { ResultSetHeader } from 'mysql2';
+import IUserCreate from '../interfaces/userCreateInterface';
+import connection from './connection';
+
+const create = async (userInfo: IUserCreate): Promise<ResultSetHeader> => {
+  const { username, classe, level, password } = userInfo;
+  const [result] = await connection.execute<ResultSetHeader>(
+    `
+  INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);`,
+    [username, classe, level, password],
+  );
+  return result;
+};
+
+export default create;
