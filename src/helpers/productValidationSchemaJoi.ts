69fd468ed27be089dc7a81deff0bc2e78e252ef0
Wellington <wellingtonmachioni@gmail.com>
2022-09-18 16:12:02 -0300

    feat(rota products): Criado schema joi para validar informações enviadas pelo usuário

---
+++ src/helpers/productValidationSchemaJoi.ts
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+const productValidationSchema = Joi.object({
+  name: Joi.string().min(3).required().messages({
+    'any.required': '"name" is required',
+    'string.min': '"name" length must be at least 3 characters long',
+  }),
+  amount: Joi.string().min(3).required().messages({
+    'any.required': '"name" is required',
+    'string.min': '"amount" length must be at least 3 characters long',
+  }),
+});
+
+export default productValidationSchema;
