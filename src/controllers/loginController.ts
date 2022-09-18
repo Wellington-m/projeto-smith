4cb90eb1869b3b310a29f97aafbed6e2e14b302d
Wellington <wellingtonmachioni@gmail.com>
2022-09-18 15:36:16 -0300

    feat(rota login): Implementado validação de usuário

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import loginService from '../services/loginService';
 
 const login = async (req: Request, res: Response): Promise<Response> => {
   const { username, password } = req.body;
   const result = await loginService.login({ username, password });
-  return res.status(StatusCodes.OK).json({ token: result });
+  if (!result) return res.status(401).json({ message: 'Username or password invalid' });
+  return res.status(200).json({ token: result });
 };
 
 export default { login };
--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -4,6 +4,7 @@ import loginModel from '../models/loginModel';
 
 const login = async (loginInfo: ILogin): Promise<string | null> => {
   const userInfo = await loginModel.getUserInfosByUsername(loginInfo.username);
+  if (userInfo.length === 0) return null;
   if (userInfo[0].password !== loginInfo.password) return null;
   const token = tokenHelper.createToken({ id: userInfo[0].id });   
   return token;
